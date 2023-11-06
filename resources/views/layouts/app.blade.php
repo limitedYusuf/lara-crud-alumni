287442f97f49e74690d8f16909e0ff86a7ded938
idiotRun <doflamingod642@gmail.com>
2023-11-06 14:49:49 +0800

    set favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <meta name="theme-color" content="#ffffff">
+    <link rel="icon" href="{{ asset('images/logo_apk.jpeg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/logo_apk.jpeg') }}" type="image/x-icon">
+    
     @vite('resources/sass/app.scss')
     @stack('css')
 </head>
