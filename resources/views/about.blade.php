b8c76bc3bca8192455d9afc99001db13ec08f9fe
idiotRun <doflamingod642@gmail.com>
2023-11-06 13:57:01 +0800

    add ID

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
             {{ __('About us') }}
         </div>
         <div class="card-body">
-            {{ __('Sample static text page') }}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam excepturi dicta, hic maiores inventore voluptatum aut harum. Quasi illo doloremque dolorum reiciendis cupiditate, libero fuga recusandae ratione expedita eum! Harum nulla sed sapiente dignissimos debitis eius, dolore eligendi, est accusamus ducimus nihil rerum voluptatum libero provident molestiae, reprehenderit saepe ratione! At harum sed necessitatibus laboriosam, maiores provident? Cumque sequi porro quos eaque, quasi eveniet fugit magnam? Doloribus autem atque, quo praesentium excepturi ex perferendis distinctio deleniti rerum voluptatum ea minus laboriosam nobis hic dignissimos non minima facilis? Voluptatum distinctio itaque voluptates est ratione placeat commodi officiis eligendi. Delectus nemo deserunt est nostrum, ducimus ab maiores explicabo quidem minima libero optio voluptatum, asperiores sapiente natus quisquam, architecto aperiam. Vero laboriosam accusamus fugit. Explicabo obcaecati voluptatibus laudantium porro doloribus assumenda fugiat quo nisi beatae, minus officia sapiente, adipisci ullam eveniet reiciendis aperiam! Soluta maxime amet nihil, laboriosam aperiam aspernatur hic a nostrum magnam possimus quas, laborum quae reiciendis omnis eligendi quos quia aliquam? Sunt quia veniam nesciunt excepturi autem minima in, quos totam, ex iste atque. Dolores a sequi reiciendis impedit corrupti, ad earum debitis vero omnis consequatur officiis nulla perferendis vitae saepe pariatur dolore natus totam odit aut deleniti repellat.
         </div>
     </div>
 @endsection
